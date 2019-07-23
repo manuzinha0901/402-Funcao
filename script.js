18cbe42027f31f9cd002dac2ef020980c226abc5
manuzinha0901 <emanuelle.sanntoslima@gmail.com>
2019-07-23 17:42:53 -0300

    Testar funcoes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-//function somar (num1, num2){
-//    console.log(num1 + num2);
-//}
+function somar (num1, num2){
+   console.log(num1 + num2);
+}
 
-//function subtrair(num1,num2){
-//    console.log(num1 - num2);
-//}
+function subtrair(num1,num2){
+   console.log(num1 - num2);
+}
 
-//function multiplicar(num1,num2){
-//    console.log(num1 * num2);
-//}
+function multiplicar(num1,num2){
+   console.log(num1 * num2);
+}
 
-//somar(10, 15);
-//somar(102,42);
-//somar(15,6);
+somar(10, 15);
+somar(102,42);
+somar(15,6);
 
-//subtrair(10,15);
-//subtrair(102,42);
-//subtrair(15,6);
+subtrair(10,15);
+subtrair(102,42);
+subtrair(15,6);
 
-//multiplicar(15,6);
+multiplicar(15,6);
 
 
